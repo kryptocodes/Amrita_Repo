77b28a38417f0cd28a682f1c97d0db52c8f23159
kryptocodes <srivatsantb@gmail.com>
2019-11-24 12:37:30 +0530

    initial_commit

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Amrita Repo</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Site Under Construction until then use <a href="https://notesatamrita.me">Notes@Amrita</a>.
         </p>
       </div>
     );
