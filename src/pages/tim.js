a13b18464e863a72783291d79f40b9363d5cd9fe
kryptocodes <srivatsantb@gmail.com>
2019-12-02 13:00:32 +0530

    Timetable fix

--- src/pages/tim.js
+++ src/pages/tim.js
@@ -32,9 +32,12 @@ class Tim extends Component {
             <option value1="3">Sem 3</option>
             <option value1="4">Sem 4</option>
             <option value1="5">Sem 5</option>
+            <option value1="6">Sem 6</option>
+            <option value1="7">Sem 7</option>
+            <option value1="8">Sem 8</option>
           </select>
         <div className="button">
-          <button onClick={this.handleClick}>View</button>
+          <button className="button" onClick={this.handleClick}/*{(f)=>this.rendertim(this.state.dept,this.state.sem)}*/>View</button>
         </div>
       </form>
     );
@@ -44,7 +47,10 @@ class Tim extends Component {
     const sp = sem;
     if(dp==="CSE"&& sp === "Sem 1")
       return  window.open("https://5.imimg.com/data5/FA/BB/MY-13366359/office-organisers-500x500.jpg", "_blank");
-   
+    else if(dp==="CSE" && sp==="Sem 2")
+      return window.open("https://image.shutterstock.com/image-vector/template-school-timetable-illustration-includes-260nw-704106805.jpg");
+      else
+      return window.open("/404");
   }
 }
 
