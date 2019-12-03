d7db64ba68d845a96fb8359151cb4e3ca0ba4a70
kryptocodes <srivatsantb@gmail.com>
2019-12-03 14:40:34 +0530

    pwa fix

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Component/App';
 import './css/index.css';
-
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+registerServiceWorker();
