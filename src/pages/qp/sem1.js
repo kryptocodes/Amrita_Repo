4805bf1ed0ea4914e1daa75cbd70f011b3bceff3
kryptocodes <srivatsantb@gmail.com>
2019-12-07 20:27:09 +0530

    Qp page:fixed layout
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/pages/qp/sem1.js
+++ src/pages/qp/sem1.js
@@ -1,75 +1,73 @@
 import React, { useState, Fragment } from 'react';
 import '../../css/qp.css';
 
-/*class sem1 extends Component {
-  state={};
-  handleClick =() =>{
-    this.renderbox(this.state.r1)
-  }
-  renderbox(r1)
-  {
-    return <h1>HEllo world</h1>
-  }
-  render() {
-    return (
-      <div>
-        <div className="Grid">
-          <article >2019</article>
-         
-         
-          <article>2018</article>
-         
-          <article>2017</article>
-          <article>2016</article>
-        </div>
-      </div>
-    );
-  }
-}
-*/
+
 function sem1() {
   const [showText, setShowText] = useState(false);
   const [showone, setshowone] = useState(false);
   const [showtwo, setshowtwo] = useState(false);
+  const [showthree, setshowthree] = useState(false);
+  const [showfour, setshowfour] = useState(false);
+  const [showfive, setshowfive] = useState(false);
   return (
     <Fragment>
+       <div className="bbx">
       <button className="buon" onClick={() => setShowText(!showText)}>
-      <article className="box" > 2019 </article>
+      <article className="box" >Engineering Mathematics - I</article>
       </button>
       {showText && <div className="content">
-          <ul>Technical English - I</ul>
-          <a href="http://www.hariganesh.com/pdf/University%20Question%20Papers/R17/M1/M1_R17_AprilMay_19.pdf" target="_blank" download>
-         <ul>Engineering Mathematics - I</ul></a>
-         <ul><a href="#">Engineering Physics - I</a></ul>
-         <ul><a href="#">Engineering Chemistry - I</a></ul>
-         <ul><a href="#">Engineering Graphics</a></ul>
-         <ul><a href="#">Problem Solving and Python Programming</a></ul>
+         <ul><a href="#">2019</a></ul>
+         <ul><a href="#">2018</a></ul>
+         <ul><a href="#">2017</a></ul>
          </div>
       }
       <button className="buon" onClick={() => setshowone(!showone)}>
-      <article > 2018 </article>
+      <article >Engineering Physics - I</article>
       </button>
       {showone && <div className="content">
-         <ul><a href="#">Technical English - I</a></ul>
-         <ul><a href="#">Engineering Mathematics - I</a></ul>
-         <ul><a href="#">Engineering Physics - I</a></ul>
-         <ul><a href="#">Engineering Chemistry - I</a></ul>
-         <ul><a href="#">Engineering Graphics</a></ul>
-         <ul><a href="#">Problem Solving and Python Programming</a></ul>
+      <ul><a href="#">2019</a></ul>
+         <ul><a href="#">2018</a></ul>
+         <ul><a href="#">2017</a></ul>
          </div>
       }
       <button className="buon" onClick={() => setshowtwo(!showtwo)}>
-      <article > 2017 </article>
+      <article>Engineering Chemistry - I</article>
       </button>
       {showtwo && <div className="content">
-         <ul><a href="#">Technical English - I</a></ul>
-         <ul><a href="#">Engineering Mathematics - I</a></ul>
-         <ul><a href="#">Engineering Physics - I</a></ul>
-         <ul><a href="#">Engineering Chemistry - I</a></ul>
-         <ul><a href="#">Engineering Graphics</a></ul>
-         <ul><a href="#">Problem Solving and Python Programming</a></ul>
+      <ul><a href="#">2019</a></ul>
+         <ul><a href="#">2018</a></ul>
+         <ul><a href="#">2017</a></ul>
          </div>
       }
+        <button className="buon" onClick={() => setshowthree(!showthree)}>
+      <article>Engineering Graphics</article>
+      </button>
+      {showthree && <div className="content">
+      <ul><a href="#">2019</a></ul>
+         <ul><a href="#">2018</a></ul>
+         <ul><a href="#">2017</a></ul>
+         </div>
+      }
+      <button className="buon" onClick={() => setshowfour(!showfour)}>
+      <article>Problem Solving and Python Programming</article>
+      </button>
+      {showfour && <div className="content">
+      <ul><a href="#">2019</a></ul>
+         <ul><a href="#">2018</a></ul>
+         <ul><a href="#">2017</a></ul>
+         </div>
+      }
+       <button className="buon" onClick={() => setshowfive(!showfive)}>
+      <article>Technical English - I</article>
+      </button>
+      {showfive && <div className="content">
+      <ul><a href="#">2019</a></ul>
+         <ul><a href="#">2018</a></ul>
+         <ul><a href="#">2017</a></ul>
+         </div>
+      }
+      
+      </div>
     </Fragment>
   );
 }
