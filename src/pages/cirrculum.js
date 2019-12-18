47416ecda1a21643fd84dac3e15c307de2edb46e
kryptocodes <srivatsantb@gmail.com>
2019-12-18 23:29:05 +0530

    Added CSE syllabus
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/pages/cirrculum.js
+++ src/pages/cirrculum.js
@@ -3,26 +3,181 @@ import { Container, Row, Col } from 'react-grid-system';
  
 function cirrculum() {
     return (
-      <Container fluid style={{ marginTop:'50px',lineHeight: '32px',backgroundColor:'white' }}>
+      <div>
+        <h1 style={{color:'white'}}>CSE</h1>
+      <Container fluid style={{ marginTop:'30px',lineHeight: '32px',backgroundColor:'black',color:'white' }}>
       <Row debug >
-        <Col debug>Semester 1 </Col>
+        <Col debug  >Semester 1 </Col>
         <Col debug>Semester 2</Col>
       </Row>
+      </Container>
+      <Container fluid style={{lineHeight: '32px',backgroundColor:'white'}}>
+      <Row debug >
+        <Col debug >Engineering Mathematics - I </Col>
+        <Col debug >Engineering Mathematics II</Col>
+      </Row>
       <Row debug>
-        <Col debug>Semester 3 </Col>
+      <Col debug>Engineering Physics - I</Col>
+      <Col debug>Physics for Information Science</Col>
+      </Row>
+      <Row debug>
+        <Col debug>Engineering Chemistry - I</Col>
+        <Col debug>Environmental Science and Engineering </Col>
+      </Row>
+      <Row debug>
+        <Col debug>Engineering Graphics</Col>
+        <Col debug>Basic Electrical, Electronics and Measurement Engineering</Col>
+      </Row>
+      <Row debug>
+        <Col debug>Problem Solving and Python Programming</Col>
+        <Col debug>Programming in C</Col>
+      </Row>
+      <Row debug>
+        <Col debug>Communicative English</Col>
+        <Col debug>Technical English</Col>
+      </Row>
+      <Row debug>
+        <Col debug>Problem Solving and Python Programming Laboratory</Col>
+        <Col debug>Engineering Practices Laboratory</Col>
+      </Row>
+      <Row debug>
+        <Col debug>Physics and Chemistry Laboratory</Col>
+        <Col debug>C Programming Laboratory</Col>
+      </Row>
+      </Container>
+      <Container fluid style={{ lineHeight: '32px',backgroundColor:'black',color:'white' }}>
+      <Row debug >
+        <Col debug  >Semester 3 </Col>
         <Col debug>Semester 4</Col>
       </Row>
+      </Container>
+      <Container fluid style={{lineHeight: '32px',backgroundColor:'white'}}>
+      <Row debug >
+        <Col debug >Discrete Mathematics</Col>
+        <Col debug >Probability and Queueing Theory</Col>
+      </Row>
       <Row debug>
-        <Col debug>Semester 5 </Col>
+      <Col debug>Digital Principles and Design</Col>
+      <Col debug>Computer Architecture</Col>
+      </Row>
+      <Row debug>
+        <Col debug>Data Structures</Col>
+        <Col debug>Database Management Systems</Col>
+      </Row>
+      <Row debug>
+        <Col debug>Object Oriented Programming</Col>
+        <Col debug>Design and Analysis of Algorithms</Col>
+      </Row>
+      <Row debug>
+        <Col debug>Communication Engineering</Col>
+        <Col debug>Operating Systems</Col>
+      </Row>
+      <Row debug>
+        <Col debug>Data Structures Laboratory</Col>
+        <Col debug>Software Engineering</Col>
+      </Row>
+      <Row debug>
+        <Col debug>Object Oriented Programming Laboratory</Col>
+        <Col debug>Database Management Systems Laboratory</Col>
+      </Row>
+      <Row debug>
+        <Col debug>Digital Systems Laboratory</Col>
+        <Col debug>Operating Systems Laboratory</Col>
+      </Row>
+      <Row debug>
+        <Col debug>Interpersonal Skills/Listening and Speaking</Col>
+        <Col debug>Advanced Reading and Writing</Col>
+      </Row>
+      </Container>
+      <Container fluid style={{ lineHeight: '32px',backgroundColor:'black',color:'white' }}>
+      <Row debug >
+        <Col debug  >Semester 5 </Col>
         <Col debug>Semester 6</Col>
       </Row>
+      </Container>
+      <Container fluid style={{lineHeight: '32px',backgroundColor:'white'}}>
       <Row debug>
-        <Col debug>Semester 7 </Col>
+        <Col debug >Algebra and Number Theory</Col>
+        <Col debug >Internet Programming</Col>
+      </Row>
+      <Row debug>
+      <Col debug>Computer Networks</Col>
+      <Col debug>Artificial Intelligence</Col>
+      </Row>
+      <Row debug>
+        <Col debug>Microprocessors and Microcontrollers</Col>
+        <Col debug>Mobile Computing</Col>
+      </Row>
+      <Row debug>
+        <Col debug>Theory of Computation</Col>
+        <Col debug>Compiler Design</Col>
+      </Row>
+      <Row debug>
+        <Col debug>Object Oriented Analysis and Design</Col>
+        <Col debug>Distributed Systems</Col>
+      </Row>
+      <Row debug>
+        <Col debug>Open Elective I</Col>
+        <Col debug>Professional Elective I</Col>
+      </Row>
+      <Row debug>
+        <Col debug>Microprocessors and Microcontrollers Laboratory</Col>
+        <Col debug>Internet Programming Laboratory</Col>
+      </Row>
+      <Row debug>
+        <Col debug>Object Oriented Analysis and Design Laboratory</Col>
+        <Col debug>Mobile Application Development Laboratory</Col>
+      </Row>
+      <Row debug>
+        <Col debug>Networks Laboratory</Col>
+        <Col debug>Mini Project</Col>
+      </Row>
+      <Row debug>
+        <Col debug></Col>
+        <Col debug>Professional Communication</Col>
+      </Row>
+      </Container>
+      <Container fluid style={{ lineHeight: '32px',backgroundColor:'black',color:'white' }}>
+      <Row debug >
+        <Col debug  >Semester 7 </Col>
         <Col debug>Semester 8</Col>
       </Row>
-    
-    
-    </Container>
+      </Container>
+      <Container fluid style={{lineHeight: '32px',backgroundColor:'white'}}>
+      <Row debug >
+        <Col debug >Principles of Management</Col>
+        <Col debug >Professional Elective IV</Col>
+      </Row>
+      <Row debug>
+      <Col debug>Cryptography and Network Security</Col>
+      <Col debug>Professional Elective V</Col>
+      </Row>
+      <Row debug>
+        <Col debug>Cloud Computing</Col>
+        <Col debug>Project Work</Col>
+      </Row>
+      <Row debug>
+        <Col debug>Open Elective II</Col>
+        <Col debug></Col>
+      </Row>
+      <Row debug>
+        <Col debug>Professional Elective II</Col>
+        <Col debug></Col>
+      </Row>
+      <Row debug>
+        <Col debug>Professional Elective III</Col>
+        <Col debug></Col>
+      </Row>
+      <Row debug>
+        <Col debug>Cloud Computing Laboratory</Col>
+        <Col debug></Col>
+      </Row>
+      <Row debug>
+        <Col debug>Security Laboratory</Col>
+        <Col debug></Col>
+      </Row>
+      </Container>
+    </div>
     )
 }
 
