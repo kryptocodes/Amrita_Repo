33f621a813cea1cb01718a6711adacd9757ce0ca
kryptocodes <srivatsantb@gmail.com>
2019-12-18 22:04:29 +0530

    Modified Layout
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/pages/cirrculum.js
+++ src/pages/cirrculum.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-grid';
-
+import { Container, Row, Col } from 'react-grid-system';
+ 
 function cirrculum() {
     return (
-<Container>
-  <Row>
-    <Col>Semester 1</Col>
-    <Col>Semester 2</Col>
-  </Row>
-  <Row>
-    <Col >Semester 3</Col>
-    <Col>Semester 4</Col>
-  </Row>
-  <Row>
-    <Col>Semester 5</Col>
-    <Col>Semester 6</Col>
-  </Row>
-  <Row>
-    <Col>Semester 7</Col>
-    <Col>Semester 8</Col>
-  </Row>
-</Container>
+      <Container fluid style={{ marginTop:'50px',lineHeight: '32px',backgroundColor:'white' }}>
+      <Row debug >
+        <Col debug>Semester 1 </Col>
+        <Col debug>Semester 2</Col>
+      </Row>
+      <Row debug>
+        <Col debug>Semester 3 </Col>
+        <Col debug>Semester 4</Col>
+      </Row>
+      <Row debug>
+        <Col debug>Semester 5 </Col>
+        <Col debug>Semester 6</Col>
+      </Row>
+      <Row debug>
+        <Col debug>Semester 7 </Col>
+        <Col debug>Semester 8</Col>
+      </Row>
+    
+    
+    </Container>
     )
 }
 
